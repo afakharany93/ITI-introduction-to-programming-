b7ef86a19ab2aa7f0331961cfa8049ab718f48e6
Ahmed El Fakharany <afakharany93@gmail.com>
2016-10-17 09:48:30 +0200

    magic box lab 2

---
+++ lab2/magic_box/MAGIC_~1.C
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<conio.h>
+#define N 5
+
+int number_of_digits(int number);
+
+
+void main (void)
+{
+	int offset = number_of_digits(N*N);
+	int col = (N-1) / 2;
+	int row = 0;
+	int value = 1;
+	//int count_row = 0, count_col = 0;
+	clrscr();
+	gotoxy(offset*(1+col), row+1);
+	printf("%d", value);
+
+	while(value < N*N)
+	{
+		value++;
+		if(value % N == 0)
+		{
+			row = (++row) ;
+			if(row> N-1) row = 0;
+		}
+		else
+		{
+			row = (--row) ;
+			col = (--col) ;
+			if(row<0) row = N-1;
+			if(col<0) col = N-1;
+		}
+		gotoxy(offset*(1+col), row+1);
+		printf("%d", value);
+	}
+	getch();
+}
+
+int number_of_digits(int number)
+{
+	int count = 0;
+	int check = 0 ;
+
+	do
+	{
+		count ++;
+		check = number%( 10^(count));
+	}while(  check != number );
+
+	return count;
+}
